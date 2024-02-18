c9851b6ade85d67b248f0dda40d0796990333d43
Graphysalis <122803388+graphysalis@users.noreply.github.com>
2024-02-18 15:56:29 +0100

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,706 @@
+var cassis = ["cassis", 73];
+var grenade = ["grenade", 83];
+var goyave = ["goyave", 68];
+var prune = ["prune", 49];
+var groseille = ["groseille", 56];
+var pomme = ["pomme", 52];
+var ananas = ["ananas", 50];
+var kiwi = ["kiwi", 61];
+var mangue = ["mangue", 60];
+var fraise = ["fraise", 33];
+var poire = ["poire", 57];
+var banane = ["banane", 89];
+var peche = ["pêche", 39];
+var abricot = ["abricot", 78];
+var cerise = ["cerise", 60];
+var orange = ["orange", 45];
+var pamplemousse = ["pamplemousse", 45];
+var datte = ["datte", 280];
+var raisin = ["raisin", 72];
+
+var fruits = [
+  raisin,
+  cerise,
+  cassis,
+  grenade,
+  goyave,
+  prune,
+  groseille,
+  pomme,
+  ananas,
+  kiwi,
+  mangue,
+  fraise,
+  poire,
+  banane,
+  peche,
+  abricot,
+  orange,
+  pamplemousse,
+  datte,
+];
+fruits = fruits.sort();
+
+var canard = ["canard", 200];
+var saucisseMorteau = ["saucisse de Morteau", 316];
+var merguez = ["merguez", 316];
+var chipolata = ["chipolata", 282];
+var saucisseFrancfort = ["saucisse de Francfort", 270];
+var lapin = ["lapin", 130];
+var veau = ["veau", 172];
+var boeuf = ["boeuf", 250];
+var porc = ["porc", 242];
+var agneau = ["agneau", 208];
+var rognons = ["rognons", 96];
+var foieDeVolaille = ["foie de volaille", 155];
+var joueDeBoeuf = ["joue de boeuf", 136];
+var poulet = ["poulet", 239];
+var sanglier = ["sanglier", 150];
+var cerf = ["cerf", 113];
+var foieDeVeau = ["foie de veau", 136];
+var dinde = ["dinde", 175];
+var oeuf = ["oeuf", 140];
+var jambonBlanc = ["jambon blanc", 135];
+var jambonCru = ["jambon cru", 230];
+var mortadelle = ["mortadelle", 300];
+var saucissonSec = ["saucisson sec", 400];
+
+var meat = [
+  saucissonSec,
+  mortadelle,
+  jambonCru,
+  jambonBlanc,
+  canard,
+  merguez,
+  chipolata,
+  saucisseMorteau,
+  saucisseFrancfort,
+  lapin,
+  oeuf,
+  veau,
+  boeuf,
+  porc,
+  agneau,
+  rognons,
+  poulet,
+  sanglier,
+  foieDeVolaille,
+  cerf,
+  joueDeBoeuf,
+  foieDeVeau,
+  dinde,
+];
+meat = meat.sort();
+
+var eau = ["eau", 0];
+var champagne = ["champagne", 81];
+var vinBlanc = ["vin blanc", 77];
+var vinRouge = ["vin rouge", 67];
+var vinRose = ["vin rosé", 70];
+var biere = ["bière", 43];
+var cafe = ["café", 0];
+var chocolatChaud = ["chocolat chaud", 76];
+var cognac = ["cognac", 200];
+var porto = ["porto", 154];
+var the = ["thé", 1];
+var sirop = ["sirop", 352];
+var jusDeFruit = ["jus de fruit", 50];
+var lait = ["lait", 48];
+var whisky = ["whisky", 249];
+var vodka = ["vodka", 237];
+var limonade = ["limonade", 40];
+var rhum = ["rhum", 248];
+var cidre = ["cidre", 40];
+var pastis = ["pastis", 274];
+var liqueur = ["liqueur", 196];
+var eauDeVie = ["eau de vie", 237];
+var gin = ["gin", 264];
+var drinks = [
+  eau,
+  champagne,
+  vinBlanc,
+  vinRouge,
+  vinRose,
+  biere,
+  cafe,
+  the,
+  lait,
+  sirop,
+  jusDeFruit,
+  chocolatChaud,
+  whisky,
+  cognac,
+  porto,
+  vodka,
+  rhum,
+  limonade,
+  pastis,
+  cidre,
+  liqueur,
+  eauDeVie,
+  gin,
+];
+
+drinks = drinks.sort();
+
+var tomate = ["tomate", 17];
+var courgette = ["courgette", 18];
+var poireau = ["poireau", 61];
+var carotte = ["carotte", 41];
+var oignon = ["oignon", 40];
+var navet = ["navet", 28];
+var poivron = ["poivron", 20];
+var endive = ["endive", 17];
+var pommeDeTerre = ["pomme de terre", 80];
+var avocat = ["avocat", 160];
+var celeri = ["céleri", 18];
+var broccoli = ["broccoli", 34];
+var betterave = ["betterave", 43];
+var chouFleur = ["chou-fleur", 25];
+var chouVert = ["chou vert", 25];
+var aubergine = ["aubergine", 25];
+var salade = ["salade verte", 15];
+var chouDeBruxelles = ["chou de Bruxelles", 43];
+var petitsPois = ["petits pois", 80];
+var mais = ["maïs", 106];
+var champignon = ["champignon", 20];
+
+var vegetables = [
+  courgette,
+  tomate,
+  mais,
+  petitsPois,
+  betterave,
+  carotte,
+  oignon,
+  celeri,
+  poireau,
+  avocat,
+  chouFleur,
+  chouVert,
+  broccoli,
+  pommeDeTerre,
+  endive,
+  navet,
+  poivron,
+  aubergine,
+  chouDeBruxelles,
+  salade,
+  champignon,
+];
+
+vegetables = vegetables.sort();
+
+var poulpe = ["poulpe", 86];
+var crevettes = ["crevettes", 94];
+var espadon = ["espadon", 172];
+var turbot = ["turbot", 118];
+var maquereau = ["maquereau", 228];
+var sardine = ["sardine", 163];
+var huitres = ["huîtres", 42];
+var sole = ["sole", 73];
+var calamar = ["calamar", 77];
+var raie = ["raie", 90];
+var colin = ["colin", 73];
+var thon = ["thon", 132];
+var rouget = ["rouget", 143];
+var cabillaud = ["cabillaud", 77];
+var bulots = ["bulots", 90];
+var moules = ["moules", 72];
+var crabe = ["crabe", 70];
+var saumon = ["saumon", 184];
+
+var seafood = [
+  crevettes,
+  colin,
+  raie,
+  bulots,
+  moules,
+  huitres,
+  sole,
+  poulpe,
+  calamar,
+  cabillaud,
+  espadon,
+  thon,
+  rouget,
+  crabe,
+  turbot,
+  maquereau,
+  sardine,
+  saumon,
+];
+seafood = seafood.sort();
+
+var ble = ["blé", 149];
+var millet = ["millet", 378];
+var avoine = ["avoine", 350];
+var orge = ["orge", 120];
+var graineDeCouscous = ["graine de coucous", 170];
+var rizBlanc = ["riz blanc", 150];
+var poisCasses = ["pois cassés", 130];
+var poisChiches = ["pois chiches", 150];
+var lentillesVertes = ["lentilles vertes", 150];
+var pates = ["pates", 130];
+
+var cereals = [
+  pates,
+  ble,
+  mais,
+  avoine,
+  lentillesVertes,
+  orge,
+  millet,
+  poisCasses,
+  poisChiches,
+  rizBlanc,
+  graineDeCouscous,
+];
+
+cereals = cereals.sort();
+
+var tartiflette = ["tartiflette", 150];
+var glace = ["glace", 180];
+var nemPoulet = ["nem au poulet", 192];
+var potAuFeu = ["pot-au-feu", 72];
+var sorbet = ["sorbet", 130];
+var cremeBrulee = ["crème brûlée", 300];
+var rizCantonais = ["riz cantonais", 141];
+var ileFlottante = ["île flottante", 140];
+var pouletAuCurry = ["poulet au curry", 120];
+var pizza = ["pizza", 250];
+var quiche = ["quiche", 300];
+var paella = ["paella", 170];
+var daube = ["daube", 130];
+var mousseAuChocolat = ["mousse au chocolat", 164];
+var tarteAuPommes = ["tarte au pommes", 280];
+var meringue = ["meringue", 400];
+var eclairAuCafe = ["éclair au café", 250];
+
+var dishes = [
+  tartiflette,
+  nemPoulet,
+  quiche,
+  pizza,
+  paella,
+  daube,
+  potAuFeu,
+  pouletAuCurry,
+  rizCantonais,
+  mousseAuChocolat,
+  cremeBrulee,
+  ileFlottante,
+  tarteAuPommes,
+  sorbet,
+  glace,
+  meringue,
+  eclairAuCafe,
+];
+
+dishes = dishes.sort();
+
+var moutarde = ["moutarde", 150];
+var huileDolive = ["huile d'olive", 900];
+var beurre = ["beurre", 750];
+var vinaigre = ["vinaigre", 20];
+var vinaigreBalsamique = ["vinaigre balsamique", 280];
+var huileDeTournesol = ["huile de tournesol", 900];
+var LaitDeCoco = ["lait de coco", 190];
+var mayonnaise = ["mayonnaise", 700];
+var ketchup = ["ketchup", 120];
+var sauceNem = ["sauce nuoc-mâm", 60];
+var sauceAuPoivre = ["sauce au poivre", 61];
+var sauceRoquefort = ["sauce Roquefort", 165];
+var SauceEchalote = ["sauce à l'échalote", 70];
+var SauceRouille = ["sauce Rouille", 360];
+var SauceTartare = ["sauce Tartare", 520];
+var coulisDeTomate = ["coulis de tomate", 25];
+var tapenade = ["tapenade", 240];
+var anchoiade = ["anchoïade", 300];
+
+var condiments = [
+  beurre,
+  moutarde,
+  vinaigre,
+  huileDolive,
+  huileDeTournesol,
+  LaitDeCoco,
+  mayonnaise,
+  ketchup,
+  sauceNem,
+  vinaigreBalsamique,
+  coulisDeTomate,
+  tapenade,
+  anchoiade,
+  sauceAuPoivre,
+  sauceRoquefort,
+  SauceRouille,
+  SauceTartare,
+  SauceEchalote,
+];
+condiments = condiments.sort();
+
+var quantity = [
+  0, 5, 10, 15, 20, 25, 50, 75, 80, 100, 150, 200, 250, 300, 350, 400, 450, 500,
+  550, 600, 650, 700, 750, 800, 850, 900, 950, 1000,
+];
+
+var food = [
+  vegetables,
+  seafood,
+  drinks,
+  meat,
+  dishes,
+  cereals,
+  fruits,
+  condiments,
+];
+
+let grammes;
+let cal1;
+let cal2;
+let cal3;
+let cal4;
+let cal5;
+let cal6;
+let cal7;
+let cal8;
+let totalCal1;
+let totalCal2;
+let totalCal3;
+let totalCal4;
+let totalCal5;
+let totalCal6;
+let totalCal7;
+let totalCal8;
+let totalCalAll;
+
+const labels = document.querySelectorAll("label");
+const selects = document.querySelectorAll("select");
+const select1 = document.querySelectorAll(".select1");
+const select2 = document.querySelectorAll(".select2");
+const options = document.querySelectorAll("option");
+const choix2 = document.querySelectorAll(".choix2");
+const foodClass = document.querySelectorAll(".food");
+const total1 = document.querySelector("#total1");
+const total2 = document.querySelector("#total2");
+const total3 = document.querySelector("#total3");
+const total4 = document.querySelector("#total4");
+const total5 = document.querySelector("#total5");
+const total6 = document.querySelector("#total6");
+const total7 = document.querySelector("#total7");
+const total8 = document.querySelector("#total8");
+const texts = document.querySelectorAll(".text");
+const totalAll = document.querySelector("#totalAll");
+const text1 = document.querySelector("#text1");
+const text2 = document.querySelector("#text2");
+const text3 = document.querySelector("#text3");
+const text4 = document.querySelector("#text4");
+const text5 = document.querySelector("#text5");
+const text6 = document.querySelector("#text6");
+const text7 = document.querySelector("#text7");
+const text8 = document.querySelector("#text8");
+const textAll = document.querySelector("#textAll");
+const choice1 = document.querySelector("#choice1");
+const choice2 = document.querySelector("#choice2");
+const choice3 = document.querySelector("#choice3");
+const choice4 = document.querySelector("#choice4");
+const choice5 = document.querySelector("#choice5");
+const choice6 = document.querySelector("#choice6");
+const choice7 = document.querySelector("#choice7");
+const choice8 = document.querySelector("#choice8");
+
+function createOption() {
+  for (let i = 0; i < vegetables.length; i++) {
+    choice1.innerHTML += `<option value="${vegetables[i]}">${vegetables[i][0]}</option>`;
+  }
+  for (let i = 0; i < seafood.length; i++) {
+    choice5.innerHTML += `<option value="${seafood[i]}">${seafood[i][0]}</option>`;
+  }
+  for (let i = 0; i < fruits.length; i++) {
+    choice3.innerHTML += `<option value="${fruits[i]}">${fruits[i][0]}</option>`;
+  }
+  for (let i = 0; i < drinks.length; i++) {
+    choice6.innerHTML += `<option value="${drinks[i]}">${drinks[i][0]}</option>`;
+  }
+  for (let i = 0; i < dishes.length; i++) {
+    choice7.innerHTML += `<option value="${dishes[i]}">${dishes[i][0]}</option>`;
+  }
+  for (let i = 0; i < cereals.length; i++) {
+    choice2.innerHTML += `<option value="${cereals[i]}">${cereals[i][0]}</option>`;
+  }
+  for (let i = 0; i < meat.length; i++) {
+    choice4.innerHTML += `<option value="${meat[i]}">${meat[i][0]}</option>`;
+  }
+  for (let i = 0; i < condiments.length; i++) {
+    choice8.innerHTML += `<option value="${condiments[i]}">${condiments[i][0]}</option>`;
+  }
+
+  selects.forEach((select) => {
+    switch (select.className) {
+      case "select2":
+        for (let i = 0; i < quantity.length; i++) {
+          select.innerHTML += `<option value="${quantity[i]}">${quantity[i]}</option>`;
+        }
+        break;
+      default:
+        null;
+        break;
+    }
+  });
+}
+
+window.addEventListener("load", () => {
+  createOption();
+  setInterval();
+});
+
+function getCal1() {
+  cal1 = choice1.value.match(/\d/g).join("");
+}
+function getCal2() {
+  cal2 = choice2.value.match(/\d/g).join("");
+}
+function getCal3() {
+  cal3 = choice3.value.match(/\d/g).join("");
+}
+function getCal4() {
+  cal4 = choice4.value.match(/\d/g).join("");
+}
+function getCal5() {
+  cal5 = choice5.value.match(/\d/g).join("");
+}
+function getCal6() {
+  cal6 = choice6.value.match(/\d/g).join("");
+}
+function getCal7() {
+  cal7 = choice7.value.match(/\d/g).join("");
+}
+function getCal8() {
+  cal8 = choice8.value.match(/\d/g).join("");
+}
+
+function getQuant1() {
+  grammes = document.getElementById("quant1").value;
+}
+function getQuant2() {
+  grammes = document.getElementById("quant2").value;
+}
+function getQuant3() {
+  grammes = document.getElementById("quant3").value;
+}
+function getQuant4() {
+  grammes = document.getElementById("quant4").value;
+}
+function getQuant5() {
+  grammes = document.getElementById("quant5").value;
+}
+function getQuant6() {
+  grammes = document.getElementById("quant6").value;
+}
+function getQuant7() {
+  grammes = document.getElementById("quant7").value;
+}
+function getQuant8() {
+  grammes = document.getElementById("quant8").value;
+}
+
+total1.addEventListener("click", () => {
+  getCal1();
+  getQuant1();
+  totalCal1 = (grammes * cal1) / 100;
+  nom = choice1.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal1);
+  console.log(grammes);
+  console.log(totalCal1);
+  console.log(nom);
+  text1.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text1.textContent = ` ${grammes}g d'${nom} apportent ${totalCal1} kcal `;
+  } else {
+    text1.textContent = ` ${grammes}g de ${nom} apportent ${totalCal1} kcal `;
+  }
+});
+
+total2.addEventListener("click", () => {
+  getCal2();
+  getQuant2();
+  totalCal2 = (grammes * cal2) / 100;
+  nom = choice2.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal2);
+  console.log(grammes);
+  console.log(totalCal2);
+  console.log(nom);
+  text2.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text2.textContent = ` ${grammes}g d'${nom} apportent ${totalCal2} kcal `;
+  } else {
+    text2.textContent = ` ${grammes}g de ${nom} apportent ${totalCal2} kcal `;
+  }
+});
+
+total3.addEventListener("click", () => {
+  getCal3();
+  getQuant3();
+  totalCal3 = (grammes * cal3) / 100;
+  nom = choice3.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal3);
+  console.log(grammes);
+  console.log(totalCal3);
+  console.log(nom);
+  text3.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text3.textContent = ` ${grammes}g d'${nom} apportent ${totalCal3} kcal `;
+  } else {
+    text3.textContent = ` ${grammes}g de ${nom} apportent ${totalCal3} kcal `;
+  }
+});
+
+total4.addEventListener("click", () => {
+  getCal4();
+  getQuant4();
+  totalCal4 = (grammes * cal4) / 100;
+  nom = choice4.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal4);
+  console.log(grammes);
+  console.log(totalCal4);
+  console.log(nom);
+  text4.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text4.textContent = ` ${grammes}g d'${nom} apportent ${totalCal4} kcal `;
+  } else {
+    text4.textContent = ` ${grammes}g de ${nom} apportent ${totalCal4} kcal `;
+  }
+});
+
+total5.addEventListener("click", () => {
+  getCal5();
+  getQuant5();
+  totalCal5 = (grammes * cal5) / 100;
+  nom = choice5.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal5);
+  console.log(grammes);
+  console.log(totalCal5);
+  console.log(nom);
+  text5.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text5.textContent = ` ${grammes}g d'${nom} apportent ${totalCal5} kcal `;
+  } else {
+    text5.textContent = ` ${grammes}g de ${nom} apportent ${totalCal5} kcal `;
+  }
+});
+
+total6.addEventListener("click", () => {
+  getCal6();
+  getQuant6();
+  totalCal6 = (grammes * cal6) / 10;
+  nom = choice6.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal6);
+  console.log(grammes);
+  console.log(totalCal6);
+  console.log(nom);
+  text6.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text6.textContent = ` ${grammes} cl d'${nom} apportent ${totalCal6} kcal `;
+  } else {
+    text6.textContent = ` ${grammes} cl de ${nom} apportent ${totalCal6} kcal `;
+  }
+});
+
+total7.addEventListener("click", () => {
+  getCal7();
+  getQuant7();
+  totalCal7 = (grammes * cal7) / 100;
+  nom = choice7.value.match(/[a-z- àéèîêûï]/gi).join("");
+  console.log(cal7);
+  console.log(grammes);
+  console.log(totalCal7);
+  console.log(nom);
+  text7.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text7.textContent = ` ${grammes}g d'${nom} apportent ${totalCal7} kcal `;
+  } else {
+    text7.textContent = ` ${grammes}g de ${nom} apportent ${totalCal7} kcal `;
+  }
+});
+
+total8.addEventListener("click", () => {
+  getCal8();
+  getQuant8();
+  totalCal8 = (grammes * cal8) / 100;
+  nom = choice8.value.match(/[a-z- àéèîêûï']/gi).join("");
+  console.log(cal8);
+  console.log(grammes);
+  console.log(totalCal8);
+  console.log(nom);
+  text8.style.backgroundColor = "rgb(97, 11, 43)";
+  if (nom[0].match(/[a|e|i|u|o|é|è|h]/g)) {
+    text8.textContent = ` ${grammes}g d'${nom} apportent ${totalCal8} kcal `;
+  } else {
+    text8.textContent = ` ${grammes}g de ${nom} apportent ${totalCal8} kcal `;
+  }
+});
+
+totalAll.addEventListener("click", () => {
+  textAll.style.backgroundColor = "rgb(83, 87, 86)";
+  textAll.style.border = "2px solid white";
+
+  if (text1.textContent == "") {
+    totalCal1 = 0;
+  }
+  if (text2.textContent == "") {
+    totalCal2 = 0;
+  }
+  if (text3.textContent == "") {
+    totalCal3 = 0;
+  }
+  if (text4.textContent == "") {
+    totalCal4 = 0;
+  }
+  if (text5.textContent == "") {
+    totalCal5 = 0;
+  }
+  if (text6.textContent == "") {
+    totalCal6 = 0;
+  }
+  if (text7.textContent == "") {
+    totalCal7 = 0;
+  }
+  if (text8.textContent == "") {
+    totalCal8 = 0;
+  }
+  totalCalAll =
+    totalCal1 +
+    totalCal2 +
+    totalCal3 +
+    totalCal4 +
+    totalCal5 +
+    totalCal6 +
+    totalCal7 +
+    totalCal8;
+  console.log(totalCalAll);
+
+  const alarm = () => {
+    const audio = new Audio();
+    audio.src = "./assets/warning.mp3";
+    audio.play();
+  };
+
+  if (totalCalAll > 2400) {
+    textAll.textContent = `ATTENTION ! ${totalCalAll} calories `;
+    textAll.style.backgroundColor = "rgb(208, 76, 95)";
+    textAll.style.boxShadow = "-3px 2px 2px 2px rgb(28, 38, 38)";
+    textAll.style.border = "2px solid transparent";
+    alarm();
+  } else if (totalCalAll > 1600) {
+    textAll.textContent = `${totalCalAll} calories au total `;
+    textAll.style.boxShadow = "-3px 2px 2px 2px rgb(28, 38, 38)";
+    textAll.style.backgroundColor = "rgb(214, 171, 31)";
+    textAll.style.border = "2px solid transparent";
+  } else {
+    textAll.textContent = `${totalCalAll} calories au total `;
+    textAll.style.boxShadow = "-3px 2px 2px 2px rgb(28, 38, 38)";
+    textAll.style.border = "2px solid transparent";
+    textAll.style.backgroundColor = "rgb(31, 214, 135)";
+  }
+});
